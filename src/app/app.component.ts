1817719ff93cd0d25bf2e438228d9cdca5c8e923
kotala-sudo <pkotala@yahoo.com>
2021-03-11 23:42:49 -0600

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import { Component } from "@angular/core";
+import { COURSES } from "../db-data";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  csci160 = COURSES[0];
-  csci161 = COURSES[1];
-  csci213 = COURSES[2];
+  showOrHide: boolean = true;
+  count: number = 0;
+  log: number[] = [];
+  onClick() {
+    this.showOrHide = !this.showOrHide;
+    this.log.push(this.count++);
+  }
 }
